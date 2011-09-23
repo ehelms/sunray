c71e264bed0e1a26c5da061541c959f9e3613798
Eric D Helms <ericdhelms@gmail.com>
2011-09-23 19:04:28 -0400

    Added sqlite database and added admin.

--- src/django-sunray/project/settings/base.py
+++ src/django-sunray/project/settings/base.py
@@ -104,9 +104,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'south'
+    'south',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
